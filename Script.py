d32b89813f94b68c4163fac761798ff1b2a36233
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2023-11-13 13:29:40 +0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ
 ✯ Mʏ Vᴇʀꜱɪᴏɴ: Grixxbot ᴠ4.5.0</b>"""
    
     SOURCE_TXT = """<b>NOTE:</b>
-- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href=https://github.com/teegrixx>TEEGRIXX</a>
+- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href=https://t.me/teegrixx>TEEGRIXX</a>
 
 <b>ᴅᴇᴠ: <a herf=https://t.me/teegrixx>teegrixxᴛɢ</a></b>"""
 
